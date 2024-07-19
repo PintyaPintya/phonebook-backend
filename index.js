9fa95dc12c555df4c10fb957dabc37d7b5810adc
PintyaPintya <omkajale09@gmail.com>
2024-07-19 15:49:19 +0530

    added dist

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 app.use(express.json());
 app.use(cors())
+app.use(express.static('dist'))
 
-morgan.token('data', function(req,res) {
-    return JSON.stringify(req.body)
-})
-app.use(morgan(':method :url :status :response-time ms - :data'))
 
 let persons = [
     { 
