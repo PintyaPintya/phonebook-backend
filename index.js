8c658836a4b6b1ecdf1c75af7c46aff82bf074a4
PintyaPintya <omkajale09@gmail.com>
2024-07-19 22:51:29 +0530

    deploy changes to prod

--- index.js
+++ index.js
@@ -67,14 +67,8 @@ const nameExists = (name) => {
     return persons.some(person => person.name === name)
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-
-    if (!body.name) {
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
 
     if(nameExists(body.name)){
         return response.status(400).json({
@@ -82,34 +76,29 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    if (!body.number) {
-        return response.status(400).json({
-            error: 'number missing'
-        })
-    }
-
     const person = new Person({
         name: body.name,
         number: body.number
     })
+    
 
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const {name,number} = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
-
-    Person.findByIdAndUpdate(request.params.id, person, {new: true})
-        .then(updatedPerson => {
-            response.json(updatedPerson)
+    Person.findByIdAndUpdate(
+        request.params.id,
+        {name,number},
+        {new:true, runValidators:true, context: 'query'}
+    )
+        .then(updatedNote => {
+            response.json(updatedNote)
         })
         .catch(error => next(error))
 })
@@ -125,6 +114,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if(error.name === 'ValidationError'){
+        return response.status(400).json({error: error.message})
     }
 
     next(error)
--- models/person.js
+++ models/person.js
@@ -13,8 +13,22 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minLength: 3,
+        required: [true, 'User name required'],
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function(v) {
+                return /\d{3}-\d{3}-\d{4}/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User number required'],
+    },
 })
 
 personSchema.set('toJSON', {
