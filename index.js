6f9f5305b9a6a41db58f63810cc159baefc1921e
PintyaPintya <omkajale09@gmail.com>
2024-07-19 15:26:44 +0530

    added morgan

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
-//const morgan = require('morgan')
+const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 app.use(express.json());
 app.use(cors())
 
-/*morgan.token('data', function(req,res) {
+morgan.token('data', function(req,res) {
     return JSON.stringify(req.body)
 })
-app.use(morgan(':method :url :status :response-time ms - :data')) */
+app.use(morgan(':method :url :status :response-time ms - :data'))
 
 let persons = [
     { 
